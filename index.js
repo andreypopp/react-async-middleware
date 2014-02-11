2b8cf03d36b5cb0926c6e7c4034ad17a28698985
Nate Wienert <natewienert@gmail.com>
2014-02-11 15:13:54 -0800

    Adding options to set additional props to send, as well as to avoid sending immediately

--- index.js
+++ index.js
@@ -3,13 +3,25 @@
 var url        = require('url');
 var ReactAsync = require('react-async');
 
-module.exports = function(component) {
+Object.prototype.extend = function(b) {
+  for (var prop in b)
+    if (b.hasOwnProperty(prop))
+      this[prop] = b[prop];
+  return this;
+};
+
+module.exports = function(component, opts) {
+  var opts = opts || { send: true };
   return function(req, res, next) {
     var path = url.parse(req.url).pathname;
-    var c = component({path: path});
+    var c = component({path: path}.extend(opts.props || {}));
     ReactAsync.renderComponentToString(c, function(err, markup) {
       if (err) return next(err);
-      res.send(markup);
+      if (opts.send) res.send(markup);
+      else {
+        res.body = markup;
+        next();
+      }
     });
-  };
-};
+  }
+}
